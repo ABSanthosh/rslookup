3ecd31e93fd269dd44fc8c45c7ef15a38964cc38
ABSanthosh <a.b.santhosh02@gmail.com>
2023-11-12 16:47:35 +0530

    feat: base calculator

--- src/routes/gpa/+page.svelte
+++ src/routes/gpa/+page.svelte
@@ -1,71 +1,82 @@
 <script lang="ts">
-  const GRADES = [
-    {
-      grade: "A",
-      points: 10,
-    },
-    {
-      grade: "A-",
-      points: 9,
-    },
-    {
-      grade: "B",
-      points: 8,
-    },
-    {
-      grade: "B-",
-      points: 7,
-    },
-    {
-      grade: "C",
-      points: 6,
-    },
-    {
-      grade: "C-",
-      points: 5,
-    },
-    {
-      grade: "D",
-      points: 4,
-    },
-    {
-      grade: "E",
-      points: 2,
-    },
-    {
-      grade: "F",
-      points: 0,
-    },
-    {
-      grade: "F*",
-      points: 0,
-    },
-  ] as const;
+  const GRADES: {
+    [key: string]: number;
+  } = {
+    A: 10,
+    "A*": 9.5,
+    "A-": 9,
+    "A-*": 8.5,
+    B: 8,
+    "B*": 7.5,
+    "B-": 7,
+    "B-*": 6.5,
+    C: 6,
+    "C*": 5.5,
+    "C-": 5,
+    "C-*": 4.5,
+    D: 4,
+    "D*": 3.5,
+    E: 2,
+    F: 0,
+    "F*": 0,
+  } as const;
 
-  $: currentGPA = 0;
-  const semesters = [
+  $: currentCGPA = 0;
+  $: creditsDone = 0;
+  $: cumulative = 0.0;
+  $: semesters = [
     {
       title: "Semester 1",
       SGPA: 0,
       courses: [
         {
           name: "Course 1",
-          credits: 4,
-          grade: "A",
-        },
-        {
-          name: "Course 2",
-          credits: 4,
-          grade: "A",
-        },
-        {
-          name: "Course 3",
-          credits: 4,
+          credits: 0,
           grade: "A",
         },
       ],
     },
   ];
+
+  $: semesters.map((item) => item.courses),
+    semesters.forEach((_, i) => calcSGPA(i));
+
+  const calcSGPA = (index: number) => {
+    // if any course has no credits, set SGPA to 0
+    if (semesters[index].courses.some((course) => !course.credits)) {
+      semesters[index].SGPA = 0;
+      return;
+    }
+
+    // sgpa = sum(credits * grade) / sum(credits)
+    const semester = semesters[index];
+    const { courses } = semester;
+    const totalCredits = courses.reduce((acc, curr) => acc + curr.credits, 0);
+    const totalGrade = courses.reduce(
+      (acc, curr) => acc + curr.credits * GRADES[curr.grade],
+      0
+    );
+    semesters[index].SGPA = Math.round((totalGrade / totalCredits) * 100) / 100;
+
+    // calculate CGPA
+    // cgpa = (currentCGPA * creditsDone) + sum((sgpa * semester credits)) / sum(semester credits) + creditsDone
+    cumulative = parseFloat(
+      (
+        (currentCGPA * creditsDone +
+          semesters.reduce(
+            (acc, curr) =>
+              acc + curr.SGPA * curr.courses.reduce((a, c) => a + c.credits, 0),
+            0
+          )) /
+        (creditsDone +
+          semesters.reduce(
+            (acc, curr) =>
+              acc + curr.courses.reduce((a, c) => a + c.credits, 0),
+            0
+          ))
+      ).toFixed(2)
+    );
+  };
 </script>
 
 <div class="GPA__header w-100">
@@ -77,18 +88,18 @@
   <aside class="GPA__result w-100">
     <h3>Your GPA</h3>
     <div class="GPA__result--item">
-      <span>Cumulative</span>
-      <span>0.00</span>
+      <h4>Cumulative</h4>
+      <h4>{cumulative}</h4>
     </div>
-    {#each semesters as item}
+    {#each semesters as item, i}
       <div class="GPA__result--item">
-        <span>{item.title}</span>
-        <span>{item.SGPA}</span>
+        <p>{item.title || `Semester ${i + 1}`}</p>
+        <p>{item.SGPA}</p>
       </div>
     {/each}
     <hr />
     <div class="GPA__result--item">
-      <span>Current</span>
+      <p>Current CGPA</p>
       <input
         class="FancyInput"
         type="number"
@@ -97,11 +108,148 @@
           // @ts-ignore
           target.select();
         }}
-        bind:value={currentGPA}
+        bind:value={currentCGPA}
+      />
+    </div>
+    <div class="GPA__result--item">
+      <p>Credits done</p>
+      <input
+        class="FancyInput"
+        type="number"
+        on:focus={({ target }) => {
+          // @ts-ignore
+          target.select();
+        }}
+        bind:value={creditsDone}
       />
     </div>
   </aside>
-  <div class="GPA__content w-100">content</div>
+  <div class="GPA__content w-100">
+    {#each semesters as sem, index}
+      <section class="GPA__semester">
+        <div class="GPA__semester--top Row--between">
+          <input
+            class="FancyInput"
+            type="text"
+            placeholder="Semester Name"
+            bind:value={semesters[index].title}
+          />
+          {#if semesters.length > 1}
+            <button
+              class="FancyButton"
+              data-icon={String.fromCharCode(58829)}
+              data-close
+              on:click={() => {
+                semesters = semesters.filter((_, i) => i !== index);
+              }}
+            />
+          {/if}
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Course</th>
+              <th>Credits</th>
+              <th>Grade</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {#each sem.courses as course, i}
+              <tr>
+                <td>
+                  <input
+                    class="FancyInput"
+                    type="text"
+                    placeholder={`Course ${i + 1}`}
+                    bind:value={course.name}
+                  />
+                </td>
+                <td>
+                  <input
+                    class="FancyInput"
+                    type="number"
+                    placeholder="Credits"
+                    on:focus={({ target }) => {
+                      // @ts-ignore
+                      target.select();
+                    }}
+                    on:change={() => calcSGPA(index)}
+                    bind:value={course.credits}
+                  />
+                </td>
+                <td>
+                  <select
+                    class="FancySelect"
+                    bind:value={course.grade}
+                    on:change={() => calcSGPA(index)}
+                  >
+                    {#each Object.keys(GRADES) as grade}
+                      <option value={grade}>{grade}</option>
+                    {/each}
+                  </select>
+                </td>
+                <td>
+                  <button
+                    class="FancyButton"
+                    disabled={semesters[index].courses.length === 1}
+                    data-icon={String.fromCharCode(58829)}
+                    data-close
+                    on:click={() => {
+                      console.log(i);
+                      semesters[index].courses = semesters[
+                        index
+                      ].courses.filter((_, j) => j !== i);
+                    }}
+                  />
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+        <button
+          class="FancyButton"
+          on:click={() => {
+            semesters[index].courses = [
+              ...semesters[index].courses,
+              {
+                name: "",
+                credits: 0,
+                grade: "A",
+              },
+            ];
+          }}
+        >
+          Add Course
+        </button>
+      </section>
+    {/each}
+    <button
+      class="FancyButton"
+      disabled={semesters.length >= 10}
+      on:click={() => {
+        if (semesters.length >= 10) return;
+        semesters = [
+          ...semesters,
+          {
+            title: semesters.length
+              ? `Semester ${semesters.length + 1}`
+              : "Semester 1",
+            SGPA: 0,
+            courses: [
+              {
+                name: "",
+                credits: 0,
+                grade: "A",
+              },
+            ],
+          },
+        ];
+      }}
+    >
+      Add Semester
+    </button>
+  </div>
 </main>
 
 <style lang="scss" src="../../styles/routes/gpa.scss"></style>
