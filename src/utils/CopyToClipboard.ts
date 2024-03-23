ab00fc34ee8e8f715c1cf5cadd15231aeb5885ec
ABSanthosh <a.b.santhosh02@gmail.com>
2024-03-24 02:12:34 +0530

    chore: Misc fixes

--- src/utils/CopyToClipboard.ts
+++ src/utils/CopyToClipboard.ts
@@ -1,6 +1,6 @@
 import { addToast } from "$lib/ToastStore";
 
 export default async function copyToClipboard(text: string) {
-  addToast({ message: "Copied to clipboard!", timeout: 1500 });
+  addToast({ message: "Copied to clipboard!", type: "success" });
   await navigator.clipboard.writeText(text);
 }
