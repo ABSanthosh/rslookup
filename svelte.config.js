4ab5b00dcac74c3536f2bf57096ccae70f9bafa1
ABSanthosh <a.b.santhosh02@gmail.com>
2024-03-24 02:23:07 +0530

    chore: change to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import preprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
