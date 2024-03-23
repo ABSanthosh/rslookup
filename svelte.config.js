83f5714c8047f84ac3b8d9407e54a0abe2abb8dd
ABSanthosh <a.b.santhosh02@gmail.com>
2024-03-24 02:25:28 +0530

    chore: pre-hook

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from "@sveltejs/adapter-auto";
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from "@sveltejs/adapter-cloudflare";
 
 import preprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
