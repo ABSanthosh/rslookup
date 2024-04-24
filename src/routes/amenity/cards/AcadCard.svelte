7dfa45cc4db050e4d5ee8c031aa33291d0c0ce3f
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-25 02:40:29 +0530

    feat: amenity acad card

---
+++ src/routes/amenity/cards/AcadCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { IAcademics } from '$types/Amenity.types';
+	import MapsSelector from '$utils/MapsSelector';
+	export const data = $$props as IAcademics;
+</script>
+
+<div class="AmenityCard">
+	<div class="Row--between AmenityCard__bottom gap-10 w-100">
+		<h3>{data.name}</h3>
+		<a
+			class="CrispButton AmenityCard__bottom--map"
+			data-icon={String.fromCharCode(58715)}
+			href={MapsSelector(data.lat, data.lng)}
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Map
+		</a>
+	</div>
+</div>
+
+<style lang="scss">
+	@import './AmenityCardBase.scss';
+</style>
