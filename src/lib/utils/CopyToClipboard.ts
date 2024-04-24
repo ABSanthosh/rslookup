2129ba9b03afe54cb2677bf7c3b20ead58e17016
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-24 20:37:18 +0530

    chore: prof util functions

---
+++ src/lib/utils/CopyToClipboard.ts
@@ -0,0 +1,6 @@
+import { addToast } from "$stores/ToastStore";
+
+export default async function copyToClipboard(text?: string) {
+  addToast({ message: "Copied to clipboard!", type: "success" });
+  await navigator.clipboard.writeText(text!);
+}
