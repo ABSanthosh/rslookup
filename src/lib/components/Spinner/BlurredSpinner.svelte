9e209325052ffcdfe6c3ecfb996bbb27c82ca983
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-24 23:26:36 +0530

    feat: blurred  spinner component

---
+++ src/lib/components/Spinner/BlurredSpinner.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Spinner from './Spinner.svelte';
+
+	export let { style } = $$props as {
+		style?: string;
+	};
+</script>
+
+<div class="BlurredSpinnerWrapper" {style}>
+	<Spinner />
+</div>
+
+<style lang="scss">
+	.BlurredSpinnerWrapper {
+		top: 0;
+		left: 0;
+		z-index: 15;
+		@include box();
+		position: absolute;
+		@include make-flex();
+		background: #00000024;
+		backdrop-filter: blur(15px);
+	}
+</style>
