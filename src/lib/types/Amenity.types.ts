f8ad88df8be04e9c6e5980e5861aa399df303ce4
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-26 19:31:23 +0530

    feat: essential card

--- src/lib/types/Amenity.types.ts
+++ src/lib/types/Amenity.types.ts
@@ -1,48 +1,55 @@
 export interface IAcademics {
-  name: string;
-  lat: string;
-  lng: string;
+	name: string;
+	lat: string;
+	lng: string;
 }
 
 export interface IAdmin {
-  name: string;
-  func: string;
-  room: string;
-  mail: string;
-  phone: string;
+	name: string;
+	func: string;
+	room: string;
+	mail: string;
+	phone: string;
 }
 
 export interface IEssentials {
-  name: string;
-  lat: string;
-  lng: string;
-  "time.from": string;
-  "time.to": string;
-  mail: string;
-  phone: string;
+	name: string;
+	lat: string;
+	lng: string;
+	'time.from': string;
+	'time.to': string;
+	mail: string;
+	phone: string;
+	MO: 'FALSE' | 'TRUE';
+	TU: 'FALSE' | 'TRUE';
+	WE: 'FALSE' | 'TRUE';
+	TH: 'FALSE' | 'TRUE';
+	FR: 'FALSE' | 'TRUE';
+	SA: 'FALSE' | 'TRUE';
+	SU: 'FALSE' | 'TRUE';
 }
 
 export interface IFood {
-  name: string;
-  lat: string;
-  lng: string;
-  "time.from": string;
-  "time.to": string;
-  mail: string;
-  phone: string;
+	name: string;
+	lat: string;
+	lng: string;
+	'time.from': string;
+	'time.to': string;
+	mail: string;
+	phone: string;
 }
 
 export interface IHostel {
-  name: string;
-  lat: string;
-  lng: string;
-  mail: string;
-  phone: string;
-  warden: string;
+	name: string;
+	lat: string;
+	lng: string;
+	mail: string;
+	phone: string;
+	warden: string;
 }
 
 export interface ISport {
-  name: string;
-  lat: string;
-  lng: string;
+	name: string;
+	lat: string;
+	lng: string;
 }
--- src/routes/amenity/cards/EssentialsCard.svelte
+++ src/routes/amenity/cards/EssentialsCard.svelte
@@ -2,52 +2,87 @@
 	import type { IEssentials } from '$types/Amenity.types';
 	import MapsSelector from '$utils/MapsSelector';
 	import copyToClipboard from '$utils/CopyToClipboard';
+	import WeekDays from '$components/WeekDays.svelte';
 
 	export const data = $$props as IEssentials;
 </script>
 
 <div class="AmenityCard">
-	<div class="Row--between gap-10 w-100">
+	<div class="Col--a-start gap-15 w-100">
 		<h3>{data.name}</h3>
+
 		{#if data['time.from'] !== '' && data['time.to'] !== ''}
-			<span data-icon={String.fromCharCode(59573)}>
-				{data['time.from']} - {data['time.to']}
-			</span>
+			<div class="AmenityCard__separator">
+				<span class="AmenityCard__separator--icon" data-icon={String.fromCharCode(59573)}>
+					Time
+				</span>
+				<hr />
+				<span class="AmenityCard__separator--content">
+					{data['time.from']} - {data['time.to']}
+				</span>
+			</div>
 		{/if}
-	</div>
-	<div class="Row--between gap-10 w-100 AmenityCard__bottom">
-		<div class="AmenityCard__contact">
-			{#if data.phone !== ''}
-				<button
-					class="CopyButton"
+		{#if data.MO !== 'FALSE' && data.TU !== 'FALSE' && data.WE !== 'FALSE' && data.TH !== 'FALSE' && data.FR !== 'FALSE' && data.SA !== 'FALSE' && data.SU !== 'FALSE'}
+			<div class="AmenityCard__separator">
+				<span class="AmenityCard__separator--icon" data-icon={String.fromCharCode(59670)}>
+					Days
+				</span>
+				<hr />
+				<WeekDays
+					days={{
+						MO: data.MO === 'TRUE' ? true : false,
+						TU: data.TU === 'TRUE' ? true : false,
+						WE: data.WE === 'TRUE' ? true : false,
+						TH: data.TH === 'TRUE' ? true : false,
+						FR: data.FR === 'TRUE' ? true : false,
+						SA: data.SA === 'TRUE' ? true : false,
+						SU: data.SU === 'TRUE' ? true : false
+					}}
+				/>
+			</div>
+		{/if}
+		{#if data.phone !== ''}
+			<div class="AmenityCard__separator">
+				<span class="AmenityCard__separator--icon" data-icon={String.fromCharCode(57520)}>
+					Phone
+				</span>
+				<hr />
+				<a
+					role="button"
+					href={'tel:' + data.phone}
 					title="Copy phone number"
-					data-icon={String.fromCharCode(57520)}
+					class="CopyButton AmenityCard__separator--content"
 					on:keydown={async () => await copyToClipboard(data.phone)}
 					on:keyup={async () => await copyToClipboard(data.phone)}
 					on:keypress={async () => await copyToClipboard(data.phone)}
 					on:click={async () => await copyToClipboard(data.phone)}
 				>
-					<span>
-						{data.phone}
-					</span>
-				</button>
-			{/if}
-			{#if data.mail !== ''}
-				<button
-					class="CopyButton"
-					title="Copy email"
-					data-icon={String.fromCharCode(57688)}
+					{data.phone}
+				</a>
+			</div>
+		{/if}
+		{#if data.mail !== ''}
+			<div class="AmenityCard__separator">
+				<span class="AmenityCard__separator--icon" data-icon={String.fromCharCode(57520)}>
+					Phone
+				</span>
+				<hr />
+				<a
+					role="button"
+					href={'mail:' + data.mail}
+					title="Copy phone number"
+					class="CopyButton AmenityCard__separator--content"
 					on:keydown={async () => await copyToClipboard(data.mail)}
 					on:keyup={async () => await copyToClipboard(data.mail)}
 					on:keypress={async () => await copyToClipboard(data.mail)}
 					on:click={async () => await copyToClipboard(data.mail)}
 				>
-					<span>
-						{data.mail}
-					</span>
-				</button>
-			{/if}
-		</div>
+					{data.mail}
+				</a>
+			</div>
+		{/if}
+	</div>
+	<div class="AmenityCard__bottom">
 		<a
 			class="CrispButton AmenityCard__bottom--map"
 			data-icon={String.fromCharCode(58715)}
@@ -62,4 +97,7 @@
 
 <style lang="scss">
 	@import './AmenityCardBase.scss';
+
+	.AmenityCard {
+	}
 </style>
