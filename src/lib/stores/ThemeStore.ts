94c459b05f9a25dd41f4724ec50190b372e6161c
ABSanthosh <a.b.santhosh02@gmail.com>
2024-04-29 01:07:40 +0530

    chore: theme fall back for cookies

--- src/lib/stores/ThemeStore.ts
+++ src/lib/stores/ThemeStore.ts
@@ -1,11 +1,13 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-const theme = writable<Theme>('dark');
+const initialValue = browser ? window.localStorage.getItem('theme') ?? 'dark' : 'dark';
+const theme = writable<Theme>(initialValue as Theme);
 const setTheme = (value: Theme) => {
 	if (browser) {
 		document.documentElement.dataset.theme = value;
 		document.cookie = `theme=${value};path=/;max-age=${60 * 60 * 24 * 365}`;
+		window.localStorage.setItem('theme', value);
 		theme.set(value);
 	}
 };
