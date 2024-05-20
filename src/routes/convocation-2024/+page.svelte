2a0115ce44c13e9cfd2fff7b59da123650f483a0
ABSanthosh <a.b.santhosh02@gmail.com>
2024-05-21 01:58:32 +0530

    feat: quick links added

--- src/routes/convocation-2024/+page.svelte
+++ src/routes/convocation-2024/+page.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
   import ConvoHeroBottom from '$images/ConvoHeroBottom.jpg';
   import ConvoHeroTop from '$images/ConvoHeroTop.jpg';
+
+  const quickLinks = [
+    {
+      name: 'Invitation',
+      link: 'https://drive.google.com/file/d/1NHYyPMeRZqZhJXxYtpsj1ZlYeKiz7gkw/view?usp=sharing'
+    },
+    {
+      name: 'Guidelines',
+      link: 'https://drive.google.com/file/d/1kcWEVolPQrb3YGm6tgZ2NCyo3ZS302dI/view?usp=sharing'
+    },
+    {
+      name: 'Itinerary',
+      link: 'https://drive.google.com/file/d/1q_bzXaN-5NSbjqAFCA5WByeri6KtgS5a/view?usp=sharing'
+    },
+    {
+      name: 'Directions to SNIoE',
+      link: 'https://maps.app.goo.gl/vyowyQAKKYst37bC7'
+    }
+  ];
 </script>
 
 <span id="convocation-2024" style="display: none; visibility: hidden;" />
@@ -11,17 +30,41 @@
     CONVOCATION 2024
     <img src={ConvoHeroBottom} alt="Convocation 2024" />
   </h1>
+
+  <section class="ConvoHome__section">
+    <h2>Quick Links</h2>
+    <ul class="ConvoHome__quickLinks">
+      {#each quickLinks as { name, link }}
+        <li>
+          <a
+            href={link}
+            target="_blank"
+            class="CrispButton"
+            data-type="golden"
+            rel="noopener noreferrer"
+          >
+            {name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </section>
 </main>
 
 <style lang="scss">
   .ConvoHome {
     @include box();
+    gap: 20px;
     @include make-flex($just: flex-start);
+
+    & > * {
+      line-height: 1;
+      font-family: 'Eczar', serif;
+    }
 
     &__hero {
       @include box();
       @include make-flex();
-      font-family: 'Eczar', serif;
       font-weight: 400;
       line-height: 100px;
       letter-spacing: 10px;
@@ -62,6 +105,37 @@
         @include box(90%, auto);
         @include respondAt(470px) {
           width: 100%;
+        }
+      }
+    }
+
+    &__section {
+      @include box();
+      padding: 20px 0;
+      gap: 20px;
+      @include make-flex($just: flex-start, $align: flex-start);
+
+      & > h2 {
+        font-family: 'Eczar', serif;
+        font-weight: 400;
+        font-size: 30px;
+        text-align: center;
+        margin: 0;
+      }
+    }
+
+    &__quickLinks {
+      gap: 10px;
+      list-style: none;
+      @include make-flex($dir: row, $just: flex-start);
+      flex-wrap: wrap;
+
+      & > li {
+        flex-grow: 1;
+        @include make-flex($just: flex-start);
+
+        & > a {
+          @include box();
         }
       }
     }
