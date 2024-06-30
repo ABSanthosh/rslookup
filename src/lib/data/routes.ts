82606b465bb3a2b3a0db807d842836cb08c52fe0
ABSanthosh <a.b.santhosh02@gmail.com>
2024-06-30 23:56:51 +0530

    chore: rearranged routes order

--- src/lib/data/routes.ts
+++ src/lib/data/routes.ts
@@ -13,6 +13,17 @@ export const ROUTES = [
       'Locate your professors on campus hassle-free. Say goodbye to wandering through hallways or frantically searching for room numbers.'
   },
   {
+    icon: 59962,
+    name: 'Labs',
+    route: 'lab',
+    separator: false,
+    showSearch: true,
+    title: "Where's My Lab?",
+    background: 'linear-gradient(-45deg, #FFC796 0%, #FF6B95 100%)',
+    description:
+      'Finding your designated lab spaces with ease. No more wandering around campus, feeling lost and bewildered.'
+  },
+  {
     icon: 59507,
     route: 'documents',
     separator: false,
@@ -23,6 +34,27 @@ export const ROUTES = [
     description: 'Essential Resources at Your Fingertips: Access Crucial Documents'
   },
   {
+    icon: 62874,
+    route: 'acad-helpdesk',
+    showSearch: false,
+    separator: true,
+    name: 'Academic Helpdesk',
+    title: "Who's in Academic POCs?",
+    background: 'linear-gradient(45deg, #E78888, #1C3AE2)',
+    description:
+      'Find all the Academic POCs in one place. Get your academic queries resolved in no time.'
+  },
+  {
+    icon: 62003,
+    route: 'student-council',
+    showSearch: false,
+    separator: false,
+    name: 'Student Council',
+    title: "Who's in Student Council?",
+    background: 'linear-gradient(225deg, #8D5CC8, #2D2856)',
+    description: 'Meet the Faces Behind Your Voice: Get to Know the Members of the Student Council!'
+  },
+  {
     icon: 59999,
     route: 'gpa',
     separator: false,
@@ -31,29 +63,6 @@ export const ROUTES = [
     title: "What's My GPA?",
     background: 'linear-gradient(90deg, #F24790, #984950)',
     description: 'Estimate your academic growth with ease. No more manual calculations.'
-  },
-  {
-    icon: 59601,
-    route: 'amenity',
-    name: 'Amenities',
-    showSearch: false,
-    separator: false,
-    isAvailable: true,
-    title: "Where's My Amenity?",
-    background: 'linear-gradient(-45deg, #2575fc 0%, #6a11cb 100%)',
-    description:
-      'Find every amenity on campus, from Academic Blocks to Cafeterias, all in one place.'
-  },
-  {
-    icon: 62874,
-    route: 'acad-helpdesk',
-    showSearch: false,
-    separator: false,
-    name: 'Academic Helpdesk',
-    title: "Who's in Academic POCs?",
-    background: 'linear-gradient(45deg, #E78888, #1C3AE2)',
-    description:
-      'Find all the Academic POCs in one place. Get your academic queries resolved in no time.'
   },
   // {
   //   icon: 60144,
@@ -66,25 +75,16 @@ export const ROUTES = [
   //   description: 'Missed a few classes? Calculate your attendance to know where you stand.'
   // },
   {
-    icon: 62003,
-    route: 'student-council',
+    icon: 59601,
+    route: 'amenity',
+    name: 'Amenities',
     showSearch: false,
-    separator: false,
-    name: 'Student Council',
-    title: "Who's in Student Council?",
-    background: 'linear-gradient(225deg, #8D5CC8, #2D2856)',
-    description: 'Meet the Faces Behind Your Voice: Get to Know the Members of the Student Council!'
-  },
-  {
-    icon: 59962,
-    name: 'Labs',
-    route: 'lab',
-    separator: false,
-    showSearch: true,
-    title: "Where's My Lab?",
-    background: 'linear-gradient(-45deg, #FFC796 0%, #FF6B95 100%)',
+    separator: true,
+    isAvailable: true,
+    title: "Where's My Amenity?",
+    background: 'linear-gradient(-45deg, #2575fc 0%, #6a11cb 100%)',
     description:
-      'Finding your designated lab spaces with ease. No more wandering around campus, feeling lost and bewildered.'
+      'Find every amenity on campus, from Academic Blocks to Cafeterias, all in one place.'
   },
   {
     icon: 59939,
